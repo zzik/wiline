44e71314a5293a9dc88f4aef988f78a75e0f07ca
zzik <edin.base@outlook.com>
2023-10-09 10:51:00 +0200

    start of work on main display component

--- client/src/Users.tsx
+++ client/src/Users.tsx
@@ -16,9 +16,40 @@ const userData = async (): Promise<User[]> => {
   const usersData = await response.json();
   return usersData;
 };
+
 // react query API and http call
 const Users: React.FC = () => {
   const { data, isLoading, isError } = useQuery({
     queryKey: ["users"],
     queryFn: userData,
   });
+
+  return (
+    <section className="display">
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : isError ? (
+        <p>Fetch failed</p>
+      ) : 
+      // implementing display-based reusability should start here
+      (
+        <ul>
+          {data.map((user) => (
+            <div key={user._id}>
+              <section className="profile-section" id="top">
+                <p>E-mail: {user.email}</p>
+                <p>Phone :{user.phoneNumber}</p>
+              </section>
+              <section className="profile-section" id="bottom">
+                <p>
+                  Name : {user.firstName} {user.lastName}
+                </p>
+              </section>
+            </div>
+          ))}
+        </ul>
+      )}
+    </section>
+  );
+};
+export default Users;
